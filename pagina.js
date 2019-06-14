11a60dd91b34899298543d655363b42a74de1084
Allphaman <Cleitoncampos2015@gmail.com>
2019-06-14 16:55:49 -0300

    finalizei a header e a barra de pesquisa

---
+++ pagina.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
+  let idolos = "CR7";
+  let btnIdolo = document.querySelector(".idolos");
+
+  
